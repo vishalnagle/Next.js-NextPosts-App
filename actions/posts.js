efd11d1937f5aee54f7bd113d434427f942e991c
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-25 23:04:17 +0530

    revalidatePath function used for caching

--- actions/posts.js
+++ actions/posts.js
@@ -44,11 +44,11 @@ export async function createPost(prevState, formData) {
         content,
         userId: 1
     })
-
+    revalidatePath("/","layout");
     redirect("/feed")
 }
 
 export async function togglePostLikeStatus(postId){
   await  updatePostLikeStatus(postId,2)
-  revalidatePath("/feed");
+  revalidatePath("/","layout");
 } 
